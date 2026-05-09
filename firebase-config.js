018510b1dc3da1e43cc6e263a51ff2cefeb76e5b
ilarisiikonen <ilari.siikonen@gmail.com>
2026-05-09 08:50:31 +0300

    fix2000

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB3n_TbIvwMe5s3WJqvef9QLoaBsN80MKI",
+  authDomain: "seppo-last-race.firebaseapp.com",
+  projectId: "seppo-last-race",
+  storageBucket: "seppo-last-race.firebasestorage.app",
+  messagingSenderId: "276355163378",
+  appId: "1:276355163378:web:96cc25215cecb59d181c6b"
+};
